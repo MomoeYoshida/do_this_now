583f23290ae9418e76a65db30362eda378a3671b
MomoeYoshida <momoe.yoshida@my.jcu.edu.au>
2021-04-14 04:54:58 +0900

    Add print.py

---
+++ lecture02/print.py
@@ -0,0 +1,12 @@
+"""
+"Do This Now"
+Write a print statement using the format method to print like:
+name =  "Monty"
+money = 73.6
+Output: Monty has $73.60
+"""
+name = "Monty"
+money = 73.6
+print("name = {}".format(name))
+print("money = {}".format(money))
+print("Output: {} has ${}".format(name, money))
