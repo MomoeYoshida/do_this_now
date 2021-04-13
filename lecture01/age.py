a13b5de0cb6e4a90c5ab0f219d8f1e25235ddd8f
MomoeYoshida <momoe.yoshida@my.jcu.edu.au>
2021-04-13 18:24:42 +0900

    Add new codes to age.py

--- lecture01/age.py
+++ lecture01/age.py
@@ -3,9 +3,18 @@
 Write Python code for a program that asks the user for their age and continues
 to do so until they enter a valid age (0 to 150). The program should then
 display the user's age
+
+Write Python code to ask the user for their age and tell them if they are an
+adult or child.
 """
 
 age = int(input("Enter your age: "))
 while not 0 <= age <= 150:
     age = int(input("Enter your age: "))
 print("Your age is {}".format(age))
+
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a child.")
