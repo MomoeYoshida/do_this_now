97e69e52bd763a75ad5bf58597c217914b200d29
MomoeYoshida <momoe.yoshida@my.jcu.edu.au>
2021-04-13 18:22:08 +0900

    Add number.py

---
+++ lecture01/number.py
@@ -0,0 +1,9 @@
+"""
+"Do This Now"
+Write Python code to ask the user for a number, and then print that number
+multiplied by 2
+"""
+
+number = int(input("Enter a number: "))
+new_number = number * 2
+print("The answer is {}".format(new_number))
