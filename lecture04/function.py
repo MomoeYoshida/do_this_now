c68f218d649d551acb5eb9ed7dca4ff2fb632419
MomoeYoshida <momoe.yoshida@my.jcu.edu.au>
2021-04-14 19:24:28 +0900

    Add get_median function to function.py

--- lecture04/function.py
+++ lecture04/function.py
@@ -2,12 +2,17 @@
 "Do This Now"
 Write a function that returns the average value of a list of numbers passed
 in to it
+
+Write a function that returns the median value (middle when sorted) of a
+list of numbers passed in to it
 """
+import statistics
 
 
 def main():
     numbers = [2, 3, 5, 7, 9]
     print("The average value of a list of numbers is {}".format(calculate_average(numbers)))
+    print("The median value of a list of numbers is {}".format(get_median(numbers)))
 
 
 def calculate_average(numbers):
@@ -17,6 +22,10 @@ def calculate_average(numbers):
         count += 1
         total += number
     return total / count
+
+
+def get_median(numbers):
+    return statistics.median(numbers)
 
 
 main()
